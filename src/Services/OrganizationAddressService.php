f3472e7b942104aac15e8119e7adf4ea90e43779
Stephen Neal <stephen.neal14@gmail.com>
2021-02-18 11:36:05 -0500

    MAKE OrganizationService & OrganizationAddressService for retrieving organization constants

---
+++ src/Services/OrganizationAddressService.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Sfneal\Users\Services;
+
+
+use Sfneal\Actions\AbstractService;
+
+class OrganizationAddressService extends AbstractService
+{
+    /**
+     * Retrieve an Organization's full address
+     *
+     * @return string|null
+     */
+    public static function full(): ?string
+    {
+        return self::street() . ', ' . self::city() . ', ' . self::state() . ' ' . self::zip();
+    }
+
+    /**
+     * Retrieve an Organization's street address
+     *
+     * @return string|null
+     */
+    public static function street(): ?string
+    {
+        return config('users.org.address.street');
+    }
+
+    /**
+     * Retrieve an Organization's city name
+     *
+     * @return string|null
+     */
+    public static function city(): ?string
+    {
+        return config('users.org.address.city');
+    }
+
+    /**
+     * Retrieve an Organization's state abbreviation
+     *
+     * @return string|null
+     */
+    public static function state(): ?string
+    {
+        return config('users.org.address.state');
+    }
+
+    /**
+     * Retrieve an Organization's zip code
+     *
+     * @return string|null
+     */
+    public static function zip(): ?string
+    {
+        return config('users.org.address.zip');
+    }
+}
---
+++ src/Services/OrganizationService.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace Sfneal\Users\Services;
+
+
+use Sfneal\Actions\AbstractService;
+
+class OrganizationService extends AbstractService
+{
+    /**
+     * Retrieve the Organization's name
+     *
+     * @return string|null
+     */
+    public static function name(): ?string
+    {
+        return config('users.org.name');
+    }
+
+    /**
+     * Retrieve the Organization's logo
+     *
+     * @return string|null
+     */
+    public static function logo(): ?string
+    {
+        return config('users.org.logo');
+    }
+
+    /**
+     * Retrieve an OrganizationAddressService instance for accessing the address
+     *
+     * @return OrganizationAddressService
+     */
+    public static function address(): OrganizationAddressService
+    {
+        return new OrganizationAddressService();
+    }
+
+    /**
+     * Retrieve the Organization's phone number
+     *
+     * @param bool $href
+     * @return string|null
+     */
+    public static function phone(bool $href = false): ?string
+    {
+        // Return a phone href
+        if ($href) {
+            return 'tel:+' . str_replace('-', '', config('users.org.phone'));
+        }
+
+        // Only return the phone number
+        return config('users.org.phone');
+    }
+
+    /**
+     * Retrieve the Organization's email address
+     *
+     * @param bool $href
+     * @return string|null
+     */
+    public static function email(bool $href = false): ?string
+    {
+        // Return a phone href
+        if ($href) {
+            return 'mailto:' . config('users.org.email');
+        }
+
+        // Only return the phone number
+        return config('users.org.email');
+    }
+}

3771d34813284bb377d504e20f55eb7a9511cf11
Stephen Neal <stephen.neal14@gmail.com>
2021-04-27 11:39:54 -0400

    ADD test methods to `RoleBuilderTest`

--- tests/Feature/Builders/RoleBuilderTest.php
+++ tests/Feature/Builders/RoleBuilderTest.php
@@ -2,13 +2,77 @@
 
 namespace Sfneal\Users\Tests\Feature\Builders;
 
+use Sfneal\Queries\RandomModelAttributeQuery;
+use Sfneal\Users\Models\Role;
+
 class RoleBuilderTest extends BuilderTestCase
 {
-    // todo: create tests
+    /**
+     * @var Role
+     */
+    protected $modelClass = Role::class;
 
     /** @test */
-    public function true_is_true()
+    public function whereType()
     {
-        $this->assertTrue(true);
+        $attribute = 'type';
+        $value = (new RandomModelAttributeQuery($this->modelClass, $attribute))->execute();
+        $model = $this->modelClass::query()->whereType($value)->get();
+
+        $this->assertContains($value, $model->pluck($attribute));
+    }
+
+    /** @test */
+    public function whereTypeUser()
+    {
+        $attribute = 'type';
+        $value = 'user';
+        $model = $this->modelClass::query()->whereTypeUser()->get();
+
+        $this->assertContains($value, $model->pluck($attribute));
+    }
+
+    /** @test */
+    public function whereName()
+    {
+        $attribute = 'name';
+        $value = (new RandomModelAttributeQuery($this->modelClass, $attribute))->execute();
+        $model = $this->modelClass::query()->whereName($value)->get();
+
+        $this->assertContains($value, $model->pluck($attribute));
+    }
+
+    /** @test */
+    public function whereNameNot()
+    {
+        $attribute = 'name';
+        $value = (new RandomModelAttributeQuery($this->modelClass, $attribute))->execute();
+        $model = $this->modelClass::query()->whereNameNot($value)->get();
+
+        $this->assertNotContains($value, $model->pluck($attribute));
+    }
+
+    /** @test */
+    public function whereNameIn()
+    {
+        $attribute = 'name';
+        $values = (new RandomModelAttributeQuery($this->modelClass, $attribute, 2))->execute();
+        $model = $this->modelClass::query()->whereNameIn($values)->get();
+
+        collect($values)->each(function (string $value) use ($model, $attribute) {
+            $this->assertContains($value, $model->pluck($attribute));
+        });
+    }
+
+    /** @test */
+    public function whereNameNotIn()
+    {
+        $attribute = 'name';
+        $values = (new RandomModelAttributeQuery($this->modelClass, $attribute, 2))->execute();
+        $model = $this->modelClass::query()->whereNameNotIn($values)->get();
+
+        collect($values)->each(function (string $value) use ($model, $attribute) {
+            $this->assertNotContains($value, $model->pluck($attribute));
+        });
     }
 }

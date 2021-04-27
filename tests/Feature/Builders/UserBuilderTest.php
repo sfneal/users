f87b2b17c739fa5afa0486c8675b53f54280a4c2
Stephen Neal <stephen.neal14@gmail.com>
2021-04-27 12:39:42 -0400

    ADD test methods to `UserBuilderTest` to test `UserBuilder` methods

--- tests/Feature/Builders/UserBuilderTest.php
+++ tests/Feature/Builders/UserBuilderTest.php
@@ -3,6 +3,7 @@
 namespace Sfneal\Users\Tests\Feature\Builders;
 
 use Sfneal\Queries\RandomModelAttributeQuery;
+use Sfneal\Users\Models\Role;
 use Sfneal\Users\Models\User;
 
 class UserBuilderTest extends BuilderTestCase
@@ -72,16 +73,6 @@ class UserBuilderTest extends BuilderTestCase
     }
 
     /** @test */
-    public function whereActive()
-    {
-        $attribute = 'status';
-        $value = (new RandomModelAttributeQuery($this->modelClass, $attribute))->execute();
-        $model = $this->modelClass::query()->whereActive($value)->get();
-
-        $this->assertContains($value, $model->pluck($attribute));
-    }
-
-    /** @test */
     public function whereRole()
     {
         $attribute = 'role_id';
@@ -138,6 +129,111 @@ class UserBuilderTest extends BuilderTestCase
 
         collect($values)->each(function (int $value) use ($models, $attribute) {
             $this->assertContains($value, $models->pluck($attribute));
+        });
+    }
+
+    /** @test */
+    public function whereRoleName()
+    {
+        $attribute = 'name';
+        $value = (new RandomModelAttributeQuery(Role::class, $attribute))->execute();
+        $models = $this->modelClass::query()
+            ->with('role')
+            ->whereRoleName($value)
+            ->get();
+
+        $this->assertIsString($value);
+        $models->each(function (User $model) use ($value) {
+            $this->assertSame($value, $model->role->name);
+        });
+    }
+
+    /** @test */
+    public function whereRoleNameNot()
+    {
+        $attribute = 'name';
+        $value = (new RandomModelAttributeQuery(Role::class, $attribute))->execute();
+        $models = $this->modelClass::query()
+            ->with('role')
+            ->whereRoleNameNot($value)
+            ->get();
+
+        $this->assertIsString($value);
+        $models->each(function (User $model) use ($value) {
+            $this->assertNotSame($value, $model->role->name);
+        });
+    }
+
+    /** @test */
+    public function whereRoleNameIn()
+    {
+        $attribute = 'name';
+        $values = (new RandomModelAttributeQuery(Role::class, $attribute, 2))->execute();
+        $models = $this->modelClass::query()
+            ->with('role')
+            ->whereRoleNameIn($values)
+            ->get();
+
+        $this->assertIsArray($values);
+        $models->each(function (User $model) use ($values) {
+            $this->assertContains($model->role->name, $values);
+        });
+    }
+
+    /** @test */
+    public function whereRoleNameNotIn()
+    {
+        $attribute = 'name';
+        $values = (new RandomModelAttributeQuery(Role::class, $attribute, 2))->execute();
+        $models = $this->modelClass::query()
+            ->with('role')
+            ->whereRoleNameNotIn($values)
+            ->get();
+
+        $this->assertIsArray($values);
+        $models->each(function (User $model) use ($values) {
+            $this->assertNotContains($model->role->name, $values);
+        });
+    }
+
+    /** @test */
+    public function whereHasRole()
+    {
+        $models = $this->modelClass::query()
+            ->whereHasRole()
+            ->get();
+
+        $models->each(function (User $model) {
+            $this->assertNotNull($model->role);
+            $this->assertInstanceOf(Role::class, $model->role);
+        });
+    }
+
+    /** @test */
+    public function whereActive()
+    {
+        $models = $this->modelClass::query()
+            ->whereActive()
+            ->get();
+
+        $models->each(function (User $model) {
+            $this->assertNotNull($model->status);
+            $this->assertIsInt($model->status);
+            $this->assertSame(1, $model->status);
+        });
+    }
+
+    /** @test */
+    public function whereInactive()
+    {
+        $models = $this->modelClass::query()
+            ->whereInactive()
+            ->get();
+
+        $models->each(function (User $model) {
+            $this->assertNotNull($model->status);
+            $this->assertIsInt($model->status);
+            $this->assertSame(0, $model->status);
         });
     }
 }

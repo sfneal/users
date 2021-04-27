08efeff0b9291ea240a5c916bda6ca6a67ac4786
Stephen Neal <stephen.neal14@gmail.com>
2021-04-27 13:07:22 -0400

    ADD test method to `UserFactoryTest`

--- tests/Feature/Factories/UserFactoryTest.php
+++ tests/Feature/Factories/UserFactoryTest.php
@@ -2,13 +2,41 @@
 
 namespace Sfneal\Users\Tests\Feature\Factories;
 
-class UserFactoryTest extends FactoriesTestCase
+use Sfneal\Testing\Utils\Interfaces\Factory\FillablesTest;
+use Sfneal\Users\Models\Role;
+use Sfneal\Users\Models\User;
+
+class UserFactoryTest extends FactoriesTestCase implements FillablesTest
 {
-    // todo: create tests
+    /**
+     * @var User
+     */
+    public $modelClass = User::class;
 
     /** @test */
-    public function true_is_true()
+    public function fillables_are_correct_types()
     {
-        $this->assertTrue(true);
+        $this->assertIsInt($this->model->role_id);
+        $role = Role::query()->find($this->model->role_id);
+        $this->assertInstanceOf(Role::class, $role);
+
+        $this->assertIsString($this->model->first_name);
+        $this->assertIsString($this->model->last_name);
+        $this->assertIsString($this->model->nickname);
+        $this->assertIsInt($this->model->nickname_preferred);
+        $this->assertIsString($this->model->title);
+
+        $this->assertIsString($this->model->email);
+        $this->assertStringContainsString('@', $this->model->email);
+
+        $this->assertIsInt($this->model->rate);
+        $this->assertIsString($this->model->phone_work);
+        $this->assertIsString($this->model->phone_mobile);
+        $this->assertIsString($this->model->fax);
+        $this->assertIsString($this->model->website);
+        $this->assertIsString($this->model->bio);
+        $this->assertIsString($this->model->username);
+        $this->assertIsString($this->model->password);
+        $this->assertIsInt($this->model->status);
     }
 }

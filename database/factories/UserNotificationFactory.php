7a34a389ff4a4be113fc217c67801d7387e44928
Stephen Neal <stephen.neal14@gmail.com>
2021-04-27 13:15:59 -0400

    ADD test methods to `UserNotificationFactoryTest`
    ADD creation of `User` models to `UserNotificationFactory`.

--- database/factories/UserNotificationFactory.php
+++ database/factories/UserNotificationFactory.php
@@ -3,16 +3,42 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
+use Sfneal\Queries\RandomModelAttributeQuery;
+use Sfneal\Users\Models\User;
 use Sfneal\Users\Models\UserNotification;
 
 class UserNotificationFactory extends Factory
 {
+    public const TYPES = [
+        'Domain\Clients\Notifications\NewInquiryNotification',
+        'Domain\Plans\Notifications\BucketChangedNotification',
+    ];
+
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
     protected $model = UserNotification::class;
+
+    /**
+     * UserNotificationFactory constructor.
+     *
+     * @param null $count
+     * @param Collection|null $states
+     * @param Collection|null $has
+     * @param Collection|null $for
+     * @param Collection|null $afterMaking
+     * @param Collection|null $afterCreating
+     * @param null $connection
+     */
+    public function __construct($count = null, ?Collection $states = null, ?Collection $has = null, ?Collection $for = null, ?Collection $afterMaking = null, ?Collection $afterCreating = null, $connection = null)
+    {
+        parent::__construct($count, $states, $has, $for, $afterMaking, $afterCreating, $connection);
+
+        self::createUserFactories();
+    }
 
     /**
      * Define the model's default state.
@@ -22,11 +48,22 @@ class UserNotificationFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => $this->faker->randomNumber(3),
-            'type' => $this->faker->randomElement([
-                'Domain\Clients\Notifications\NewInquiryNotification',
-                'Domain\Plans\Notifications\BucketChangedNotification',
-            ]),
+            'user_id' => (new RandomModelAttributeQuery(User::class, 'id'))->execute(),
+            'type' => $this->faker->randomElement(self::TYPES),
         ];
+    }
+
+    /**
+     * Create User model factories if none exist.
+     *
+     * @return void
+     */
+    private static function createUserFactories(): void
+    {
+        if (!User::query()->count()) {
+            User::factory()
+                ->count(10)
+                ->create();
+        }
     }
 }
--- tests/Feature/Factories/UserNotificationFactoryTest.php
+++ tests/Feature/Factories/UserNotificationFactoryTest.php
@@ -2,13 +2,27 @@
 
 namespace Sfneal\Users\Tests\Feature\Factories;
 
-class UserNotificationFactoryTest extends FactoriesTestCase
+use Database\Factories\UserNotificationFactory;
+use Sfneal\Testing\Utils\Interfaces\Factory\FillablesTest;
+use Sfneal\Users\Models\User;
+use Sfneal\Users\Models\UserNotification;
+
+class UserNotificationFactoryTest extends FactoriesTestCase implements FillablesTest
 {
-    // todo: create tests
+    /**
+     * @var UserNotification
+     */
+    public $modelClass = UserNotification::class;
 
     /** @test */
-    public function true_is_true()
+    public function fillables_are_correct_types()
     {
-        $this->assertTrue(true);
+        $this->assertIsInt($this->model->user_id);
+        $user = User::query()->find($this->model->user_id);
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($user, $this->model->user);
+
+        $this->assertIsString($this->model->type);
+        $this->assertContains($this->model->type, UserNotificationFactory::TYPES);
     }
 }

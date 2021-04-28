7df7472b5f7e0fe3bb0d148582f7858a8f58e00b
Stephen Neal <stephen.neal14@gmail.com>
2021-04-28 12:21:28 -0400

    REFORMAT todo

--- tests/Feature/Queries/UserListQueryTest.php
+++ tests/Feature/Queries/UserListQueryTest.php
@@ -2,11 +2,34 @@
 
 namespace Sfneal\Users\Tests\Feature\Queries;
 
+use Sfneal\Queries\RandomModelAttributeQuery;
+use Sfneal\Testing\Utils\Traits\CreateRequest;
+use Sfneal\Users\Models\User;
 use Sfneal\Users\Tests\TestCase;
 
 class UserListQueryTest extends TestCase
 {
-    // todo: create tests
+    // todo: fix issue with sql 'concat' method not being available
+
+    use CreateRequest;
+
+    /**
+     * @var string
+     */
+    private $userName;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->userName = User::query()->find(
+            (new RandomModelAttributeQuery(User::class, 'id'))->execute()
+        )->name;
+    }
 
     /** @test */
     public function true_is_true()

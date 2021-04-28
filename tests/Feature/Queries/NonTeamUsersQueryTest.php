c57ffb878af8c01b09d1bc12d2d3830e3b59a2aa
Stephen Neal <stephen.neal14@gmail.com>
2021-04-28 11:40:47 -0400

    ADD test method to `NonTeamUsersQueryTest`

--- tests/Feature/Queries/NonTeamUsersQueryTest.php
+++ tests/Feature/Queries/NonTeamUsersQueryTest.php
@@ -2,13 +2,31 @@
 
 namespace Sfneal\Users\Tests\Feature\Queries;
 
-class NonTeamUsersQueryTest extends QueriesTestCase
-{
-    // todo: create tests
+use Illuminate\Database\Eloquent\Collection;
+use Sfneal\Users\Models\User;
+use Sfneal\Users\Queries\NonTeamUsersQuery;
+use Sfneal\Users\Tests\TestCase;
 
+class NonTeamUsersQueryTest extends TestCase
+{
     /** @test */
-    public function true_is_true()
+    public function query_returns_non_team_users()
     {
-        $this->assertTrue(true);
+        $expected = User::query()
+            ->withInactive()
+            ->doesntHave('team')
+            ->orderBy('last_name', 'asc')
+            ->orderBy('first_name', 'asc')
+            ->get();
+
+        $result = (new NonTeamUsersQuery())->execute();
+
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertEquals($expected->count(), $result->count());
+        $this->assertEquals($expected->pluck('id'), $result->pluck('id'));
+        $this->assertEquals($expected, $result);
+        $result->each(function (User $user) {
+            $this->assertNull($user->team);
+        });
     }
 }

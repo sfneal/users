c24b824de72acdeab5826dc5cf0b9a763a9f9150
Stephen Neal <stephen.neal14@gmail.com>
2021-04-29 11:20:44 -0400

    CUT `true_is_true()` test methods from incomplete tests to decrease runtime

--- tests/Unit/OrganizationServiceTest.php
+++ tests/Unit/OrganizationServiceTest.php
@@ -7,10 +7,4 @@ use Sfneal\Users\Tests\TestCase;
 class OrganizationServiceTest extends TestCase
 {
     // todo: create tests
-
-    /** @test */
-    public function true_is_true()
-    {
-        $this->assertTrue(true);
-    }
 }
